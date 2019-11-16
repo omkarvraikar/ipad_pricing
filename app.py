70b013921bac6d609e13ca52a5dc680fa0be93b6
omkarvraikar <57831033+omkarvraikar@users.noreply.github.com>
2019-11-16 19:54:59 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
 
     output = prediction[0]
 
-    return render_template('index.html', prediction_text='Predicted Price of ipad ==>> $ {}'.format(output))
+    return render_template('index.html', prediction_text='Price of ipad is $ {}'.format(output))
 
 
 if __name__ == "__main__":
